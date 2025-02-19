423092f93b18c207cef4f6285f0cd20ee0e878f4
parkdohyeon0428 <pkppfh@naver.com>
2025-02-19 14:56:38 +0900

    Update weather_script.py

--- weather_script.py
+++ weather_script.py
@@ -3,7 +3,7 @@ import csv
 from datetime import datetime
 import os
 
-API_KEY = "OPENWEATHER_API_KEY"
+API_KEY = "YOUR KEY"
 CITY = "Seoul"
 URL = f"http://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric"
 
@@ -15,10 +15,12 @@ description = data["weather"][0]["description"]
 timezone = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 print(temp, humidity, description, timezone)
 
+# 위의 4개의 데이터를 가지는 csv파일 생성!!
 csv_filename = "seoul_weather.csv"
 header = ["timezone", "temp", "humidity", "description"]
 
-
+# csv가 존재하면 -> True
+# csv가 존재하지 않으면 -> False
 file_exist = os.path.isfile(csv_filename)
 
 # mode = "a" -> if not is file -> create
@@ -26,10 +28,10 @@ file_exist = os.path.isfile(csv_filename)
 with open(csv_filename, "a", newline="") as file:
     writer = csv.writer(file)
 
-    # 파일 존재 x -> header 가 없다
+    # 파일이 존재하지 않는다 -> 헤더가 없다!!
     if not file_exist:
         writer.writerow(header)
 
     writer.writerow([timezone, temp, humidity, description])
 
-    print("csv 저장 완료")
+    print("csv 저장 완료!!")
